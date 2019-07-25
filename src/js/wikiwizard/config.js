d98276978b0b0c2a2ebdf1c0258d5330bcfdc103
Alexander Milde <alex@finekost.com>
2019-07-25 12:01:32 +0200

    add anchor links

--- src/js/wikiwizard/config.js
+++ src/js/wikiwizard/config.js
@@ -1,26 +1,32 @@
 export const parseable_pages = [
   {
     lang: 'de',
-    page: 'Liste von Sportarten'
+    name: 'Liste von Sportarten',
+    url_snip: 'Liste_von_Sportarten',
   },
   {
     lang: 'de',
-    page: 'Liste der Staaten der Erde'
+    name: 'Liste der Staaten der Erde',
+    url_snip: 'Liste_der_Staaten_der_Erde',
   },
   {
     lang: 'de',
-    page: 'Liste_erfolgreicher_Filme'
+    name: 'Liste erfolgreicher Filme',
+    url_snip: 'Liste_erfolgreicher_Filme',
   },
   {
     lang: 'de',
-    page: 'Hamburger_SV'
+    name: 'Hamburger SV',
+    url_snip: 'Hamburger_SV',
   },
   {
     lang: 'en',
-    page: 'List of FIFA World Cup winners'
+    name: 'List of FIFA World Cup winners',
+    url_snip: 'List_of_FIFA_World_Cup_winners',
   },
   {
     lang: 'en',
-    page: 'List of Presidents of the United States'
+    name: 'List of Presidents of the United States',
+    url_snip: 'List_of_Presidents_of_the_United_States',
   },
 ]
