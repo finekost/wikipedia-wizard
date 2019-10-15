ffa9b39584fc3432732600220d63b4d71cdb7f53
Alexander Milde <alex@finekost.com>
2019-10-15 16:24:28 +0200

    typo

--- pages/content/about.vue
+++ pages/content/about.vue
@@ -4,7 +4,7 @@
       <h1 class="text-2xl font-medium leading-tight">A Wikipedia list to JSON converter</h1>
       <h2 class="text-2xl mb-8 leading-tight">For anyone in need of some structured data</h2>
       <p>Wiki wizard can parse several wikipedia pages and transform the data into json.</p>
-      <p>You need a list of all the US Presidents or all countries with capitals? Wiki wizzard parses the wikipedia page and generates a JSON file for you.</p>
+      <p>You need a list of all the US Presidents or all countries with capitals? Wiki wizard parses the wikipedia page and generates a JSON file for you.</p>
     </div>
   </div>
 </template>
